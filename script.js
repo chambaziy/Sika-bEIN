7833607d795711028b92a6c0a2158ce4fefb1955
chambaziy <chambaziy@gmail.com>
2026-08-17 15:58:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
 
+darkModeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        darkModeBtn.innerHTML = "☀️ DEACTIVATE DARK MODE";
+
+    } else {
+
+        darkModeBtn.innerHTML = "🌙 ACTIVATE DARK MODE";
+    }
+
+});
+
+
+function openChannel(channel) {
+
+    alert("BEIN SPORTS " + channel + " selected");
+
+}
